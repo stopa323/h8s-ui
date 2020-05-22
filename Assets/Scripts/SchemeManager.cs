c52abd7a9a09991c5ee8743f77340cd0f3257058
Bartek Stopa <stopa323@gmail.com>
2020-05-22 21:49:04 +0200

    Spawn node

---
+++ Assets/Scripts/SchemeManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SchemeManager : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject nodePrefab;
+
+    private bool isSpawningNode = false;
+    private RectTransform canvasRt;
+
+    private GameObject spawningNode;
+    private RectTransform spawningNodeRt;
+
+    private void Awake()
+    {
+        canvasRt = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (!isSpawningNode) return;
+
+        UpdateNodePosition(-30f, 30f);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!isSpawningNode) return;
+
+        if (PointerEventData.InputButton.Left == eventData.button) SpawnNode(); else DiscartNode();
+    }
+
+    public void StartNodeSpawning()
+    {
+        spawningNode = Instantiate(nodePrefab, transform);
+        spawningNodeRt = spawningNode.GetComponent<RectTransform>();
+        isSpawningNode = true;
+    }
+
+    private void DiscartNode()
+    {
+        Destroy(spawningNode);
+        spawningNode = null;
+        spawningNodeRt = null;
+        isSpawningNode = false;
+    }
+
+    private void SpawnNode()
+    {
+        spawningNode = null;
+        spawningNodeRt = null;
+        isSpawningNode = false;
+    }
+
+    private void UpdateNodePosition(float xOffset = 0, float yOffset = 0)
+    {
+        var new_position = Utils.ScreenToCanvasPosition(
+            new Vector2(Input.mousePosition.x + xOffset, 
+            Input.mousePosition.y + yOffset), canvasRt);
+
+        spawningNodeRt.anchoredPosition = new_position;
+    }
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class Utils 
+{
+    public static Vector2 ScreenToCanvasPosition(
+        Vector2 screenPos, RectTransform canvasRt)
+    {
+        if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvasRt, screenPos, null, out Vector2 localCursor))
+            return Vector2.zero;
+
+        var xpos = localCursor.x + canvasRt.rect.width / 2;
+        var ypos = localCursor.y - canvasRt.rect.height / 2;
+
+        return new Vector2(xpos, ypos);
+    }
+}
