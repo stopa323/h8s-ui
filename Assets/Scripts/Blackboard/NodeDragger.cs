23e6d46307cb8f533d1927c6be0febef557dfea3
Bartek Stopa <stopa323@gmail.com>
2020-06-11 12:18:50 +0200

    Play zoom in/out animation while dragging node
    #6

--- Assets/Scripts/Blackboard/NodeDragger.cs
+++ Assets/Scripts/Blackboard/NodeDragger.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace h8s
@@ -6,6 +7,11 @@ namespace h8s
     public class NodeDragger : MonoBehaviour, IBeginDragHandler,
         IEndDragHandler, IDragHandler
     {
+        [Header("Drag parameters")]
+        [SerializeField] private const float ALPHA_FACTOR = .8f;
+        [SerializeField] private const float SCALE_FACTOR = .9f;
+        [SerializeField] private const float SCALE_ANIMATION_TIME = .1f;
+
         private RectTransform rectTransform;
         private CanvasGroup canvasGroup;
         private Node parentNode;
@@ -19,7 +25,8 @@ namespace h8s
 
         public void OnBeginDrag(PointerEventData eventData)
         {
-            canvasGroup.alpha = .6f;
+            canvasGroup.alpha = ALPHA_FACTOR;
+            StartCoroutine(ScaleAnimation());
         }
 
         public void OnDrag(PointerEventData eventData)
@@ -31,6 +38,26 @@ namespace h8s
         public void OnEndDrag(PointerEventData eventData)
         {
             canvasGroup.alpha = 1.0f;
+            StartCoroutine(ScaleAnimation(true));
+        }
+
+        IEnumerator ScaleAnimation(bool increase = false)
+        {
+            var scale = increase ? 1f : SCALE_FACTOR;
+            var srcScale = rectTransform.localScale;
+            var destScale = new Vector3(scale, scale);
+            var elapsedTime = 0f;
+
+            while (elapsedTime < SCALE_ANIMATION_TIME)
+            {
+                rectTransform.localScale = Vector3.Lerp(srcScale, destScale, (elapsedTime / SCALE_ANIMATION_TIME));
+                elapsedTime += Time.deltaTime;
+
+                yield return null;
+            }
+
+            rectTransform.localScale = destScale;
+            yield return null;
         }
     }
 }
